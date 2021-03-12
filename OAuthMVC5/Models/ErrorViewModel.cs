b5c55d4d27bb686cd3437279ee6a2eaceef2f3cc
Thomas (Noit) <tba@live.dk>
2021-03-12 13:28:21 +0100

    Add project files.

---
+++ OAuthMVC5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OAuthMVC5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
